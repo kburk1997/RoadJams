6355fa0cda9fd733526dc82a97d3b389a314c3ee
kburk1997 <kburk97@optonline.net>
2016-10-28 18:30:27 -0400

    add client id

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+var client_id = 'CLIENT_ID'; // Your client id
+var client_secret = 'CLIENT_SECRET'; // Your secret
+var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+
 /* Load the HTTP library */
 var http = require("http");
 
